6e1865fad9402c9fdf9a927ab5d710f84e2ad8c4
IsaacScheff <isaac1996@comcast.net>
2025-02-24 18:37:50 -0500

    Squares now colored according to new enum

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <map>
+#include <tuple>
 
 constexpr int WINDOW_WIDTH = 750;
 constexpr int WINDOW_HEIGHT = 750;
@@ -8,6 +10,24 @@ constexpr float INNER_BLOCK_WIDTH = 32;
 
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
+
+enum squareColor  {
+    RED,
+    BLUE,
+    GREEN,
+    YELLOW,
+    PURPLE,
+    ORANGE
+};
+
+std::map<squareColor, std::tuple<int, int, int>> colorToRGB = {
+    {RED, {255, 0, 0}},
+    {GREEN, {0, 255, 0}},
+    {BLUE, {0, 0, 255}},
+    {YELLOW, {255, 255, 0}},
+    {PURPLE, {128, 0, 128}},
+    {ORANGE, {250, 156, 28}}
+};
 
 bool initialize() {
     if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -38,14 +58,16 @@ void shutdown() {
     SDL_Quit();
 }
 
-void draw_tetris_square(SDL_Renderer* renderer, float x, float y) { //TODO: arg for either place in the grid or coordinates on screen to render square
-    
-    SDL_FRect outline = {x, y, OUTER_BLOCK_WIDTH, OUTER_BLOCK_WIDTH}; // x, y, width, height
+void draw_tetris_square(SDL_Renderer* renderer, float x_cor, float y_cor, squareColor sColor) { //TODO: arg for either place in the grid or coordinates on screen to render square
+    int rgbFirst = std::get<0>(colorToRGB[sColor]);
+    int rgbSecond = std::get<1>(colorToRGB[sColor]);
+    int rgbThird = std::get<2>(colorToRGB[sColor]);
+    SDL_FRect outline = {x_cor, y_cor, OUTER_BLOCK_WIDTH, OUTER_BLOCK_WIDTH}; // x, y, width, height
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // white color
     SDL_RenderFillRect(renderer, &outline);
 
-    SDL_FRect rect = {x + 4, y + 4, INNER_BLOCK_WIDTH, INNER_BLOCK_WIDTH}; // x, y, width, height
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Red color
+    SDL_FRect rect = {x_cor + 4, y_cor + 4, INNER_BLOCK_WIDTH, INNER_BLOCK_WIDTH}; // x, y, width, height
+    SDL_SetRenderDrawColor(renderer, rgbFirst, rgbSecond, rgbThird, 255);
     SDL_RenderFillRect(renderer, &rect);
 }
 
@@ -67,10 +89,10 @@ int main(int argc, char* args[]) {
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
         
-        draw_tetris_square(renderer, 100, 100);        /*//draw a square*/
-        draw_tetris_square(renderer, 200, 200);        /*//draw a square*/
-        draw_tetris_square(renderer, 200, 100);        /*//draw a square*/
-        draw_tetris_square(renderer, 100, 200);        /*//draw a square*/
+        draw_tetris_square(renderer, 100, 100, RED);
+        draw_tetris_square(renderer, 200, 200, BLUE);       
+        draw_tetris_square(renderer, 200, 100, GREEN);        
+        draw_tetris_square(renderer, 100, 200, PURPLE);        
 
         // Update screen
         SDL_RenderPresent(renderer);
